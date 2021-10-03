b992ce677e9f2df4a2839aba526605809163b018
Hasan <81067657+Hasan-bit-droid@users.noreply.github.com>
2021-10-03 21:52:56 +0300

    My third Homework
    My third Homework

---
+++ 2021-10-03-Homework-3/domashka 10/Source.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	// пока не смог решать
+
+	return EXIT_SUCCESS;
+}
---
+++ 2021-10-03-Homework-3/domashka 8/Source.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	int n = 0;
+	int s = 0;
+	cin >> n;
+
+	for (int i = 1; i <= n; ++i)
+	{
+		int x = 0;
+		cin >> x;
+		s += x;
+	}
+	cout << s;
+
+	return EXIT_SUCCESS;
+}
